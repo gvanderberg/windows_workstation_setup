c0b85054137186814f449f5d923e64ba6a329342
Gareth van der Berg <gvanderberg@titansoftware.co.za>
2019-08-21 20:41:44 +0200

    iis only

--- Setup-Workstation.ps1
+++ Setup-Workstation.ps1
@@ -18,52 +18,39 @@
 # choco install vnc-viewer -y
 # choco install vscode -y
 # choco install winscp -y
-choco install visualstudio2019enterprise -y
-# choco install visualstudio2019-workload-manageddesktop -y
-# choco install visualstudio2019-workload-netcoretools -y
-# # choco install visualstudio2019-workload-netcorebuildtools -y
-# choco install visualstudio2019-workload-netcrossplat -y
-# choco install visualstudio2019-workload-node -y
-# # choco install visualstudio2019-workload-nodebuildtools -y
-# choco install visualstudio2019-workload-netweb -y
-# choco install visualstudio2019-workload-webbuildtools -y
+# choco install visualstudio2019enterprise --package-parameters "--add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetCrossPlat --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Node --passive --locale en-US" -y --force
 
-# #---
-# choco install IIS-WebServerRole --source WindowsFeatures
+#---
+choco install IIS-WebServerRole --source WindowsFeatures
 
-# #---
-# choco install IIS-ApplicationDevelopment --source WindowsFeatures
+#---
+choco install IIS-ApplicationDevelopment --source WindowsFeatures
 
-# choco install IIS-NetFxExtensibility45 --source WindowsFeatures
-# choco install IIS-ASPNET45 --source WindowsFeatures
-# #choco install IIS-ISAPIExtensions --source WindowsFeatures
-# #choco install IIS-ISAPIFilter --source WindowsFeatures
-# choco install IIS-WebSockets --source WindowsFeatures
-# #---
-# choco install IIS-CommonHttpFeatures --source WindowsFeatures
+choco install IIS-NetFxExtensibility45 --source WindowsFeatures
+choco install IIS-ASPNET45 --source WindowsFeatures
+choco install IIS-WebSockets --source WindowsFeatures
+#---
+choco install IIS-CommonHttpFeatures --source WindowsFeatures
 
-# choco install IIS-DefaultDocument --source WindowsFeatures
-# choco install IIS-DirectoryBrowsing --source WindowsFeatures
-# #choco install IIS-HttpErrors --source WindowsFeatures
-# choco install IIS-HttpRedirect --source WindowsFeatures
-# #choco install IIS-StaticContent --source WindowsFeatures
-# #---
-# choco install IIS-HealthAndDiagnostics --source WindowsFeatures
+choco install IIS-DefaultDocument --source WindowsFeatures
+choco install IIS-DirectoryBrowsing --source WindowsFeatures
+choco install IIS-HttpRedirect --source WindowsFeatures
+#---
+choco install IIS-HealthAndDiagnostics --source WindowsFeatures
 
-# choco install IIS-CustomLogging --source WindowsFeatures
-# #choco install IIS-HttpLogging --source WindowsFeatures
-# choco install IIS-LoggingLibraries --source WindowsFeatures
-# choco install IIS-ODBCLogging --source WindowsFeatures
-# choco install IIS-HttpTracing --source WindowsFeatures
-# #---
-# choco install IIS-Performance --source WindowsFeatures
+choco install IIS-CustomLogging --source WindowsFeatures
+choco install IIS-LoggingLibraries --source WindowsFeatures
+choco install IIS-ODBCLogging --source WindowsFeatures
+choco install IIS-HttpTracing --source WindowsFeatures
+#---
+choco install IIS-Performance --source WindowsFeatures
 
-# choco install IIS-HttpCompressionDynamic --source WindowsFeatures
-# choco install IIS-HttpCompressionStatic --source WindowsFeatures
-# #---
-# choco install IIS-Security --source WindowsFeatures
+choco install IIS-HttpCompressionDynamic --source WindowsFeatures
+choco install IIS-HttpCompressionStatic --source WindowsFeatures
+#---
+choco install IIS-Security --source WindowsFeatures
 
-# choco install IIS-BasicAuthentication --source WindowsFeatures
-# choco install IIS-WindowsAuthentication --source WindowsFeatures
-# #---
-# choco install TelnetClient --source WindowsFeatures
+choco install IIS-BasicAuthentication --source WindowsFeatures
+choco install IIS-WindowsAuthentication --source WindowsFeatures
+#---
+choco install TelnetClient --source WindowsFeatures
