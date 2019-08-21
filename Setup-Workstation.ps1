ff096ef3745639a3f9d81f9c7e8ff44b93f7c7c5
Gareth van der Berg <gareth.shaun@gmail.com>
2019-08-21 21:01:48 +0200

    remove vnc-viewer

--- Setup-Workstation.ps1
+++ Setup-Workstation.ps1
@@ -14,7 +14,6 @@ choco install rdcman -y
 choco install slack -y
 choco install sourcetree -y
 choco install terraform -y
-choco install vnc-viewer -y
 choco install vscode -y
 choco install winscp -y
 choco install visualstudio2019enterprise --package-parameters "--add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetCrossPlat --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Node --passive --locale en-US" -y
