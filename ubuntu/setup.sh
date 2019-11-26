443c3cdf95509afa6b26afd37337dadc83fe07a0
Gareth van der Berg <gareth.shaun@gmail.com>
2019-11-26 11:04:03 +0200

    Update setup.sh

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# Install Etcher
+# Install balenaEtcher
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
 echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
 apt-get update && \
     apt-get install -y balena-etcher-electron
+rm -rf /var/lib/apt/lists/* 
+rm -rf /etc/apt/sources.list.d/*
+
+# Install GoogleChrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - 
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list 
+apt-get update && \
+    apt-get install -y google-chrome-stable
 rm -rf /var/lib/apt/lists/* 
 rm -rf /etc/apt/sources.list.d/*
 
